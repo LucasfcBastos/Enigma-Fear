ea053cdf840a23dd9580ee8046a9a1369223aba0
LucasfcBastos <lucas.software.engineering@gmail.com>
2024-06-24 19:20:37 -0300

    adjust and create menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+    
     var menuLinks = document.querySelectorAll('.menu a');
     menuLinks.forEach(function(menuLink) {
         menuLink.addEventListener('click', function(event) {
@@ -14,4 +15,17 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         });
     });
+
+    var btnExp = document.querySelector('#click-expend');
+    var menuSide = document.querySelector('.menu_side');
+    var btnSair = document.querySelector('#click-exit');
+
+    btnExp.addEventListener('click', function() {
+        menuSide.classList.add('expend');
+    });
+
+    btnSair.addEventListener('click', function() {
+        menuSide.classList.remove('expend');
+    });
+
 });
