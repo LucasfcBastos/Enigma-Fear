09e9ec561ef3a5dde79214b925462dd7bdd339c0
LucasfcBastos <lucas.software.engineering@gmail.com>
2024-07-02 15:48:47 -0300

    I've just done the whole part for sale

---
+++ src/js/link.js
@@ -0,0 +1,23 @@
+// game
+
+function game1() {
+    window.location.href = 'https://www.nuuvem.com/br-pt/item/enigma-do-medo';
+}
+
+function game2() {
+    window.location.href = 'https://www.nuuvem.com/br-pt/item/enigma-do-medo-bundle-do-detetive';
+}
+
+function game3() {
+    window.location.href = 'https://www.nuuvem.com/br-pt/item/enigma-do-medo-bundle-do-medo';
+}
+
+// product
+
+function Lolja() {
+    window.location.href = 'https://www.lolja.com.br/ordem-cellbit';
+}
+
+function Jambo() {
+    window.location.href = 'https://jamboeditora.com.br/?post_type=product&s=ordem+paranormal';
+}
