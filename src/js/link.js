3d0e651210be7fd43360d9eccdefc73cd716e312
LucasfcBastos <lucas.software.engineering@gmail.com>
2024-07-05 00:48:13 -0300

    project finished, now just making adjustments

--- src/js/link.js
+++ src/js/link.js
@@ -21,3 +21,61 @@ function Lolja() {
 function Jambo() {
     window.location.href = 'https://jamboeditora.com.br/?post_type=product&s=ordem+paranormal';
 }
+
+// cellbit
+
+function Cell_X() {
+    window.location.href = 'https://x.com/cellbit';
+}
+
+function Cell_Inst() {
+    window.location.href = 'https://www.instagram.com/cellbitos/?hl=pt-br';
+}
+
+function Cell_You() {
+    window.location.href = 'https://www.youtube.com/@CellbitLives';
+}
+
+function Cell_Twi() {
+    window.location.href = 'https://www.twitch.tv/cellbit';
+}
+
+// ordem
+
+function Ordem_X() {
+    window.location.href = 'https://x.com/OrdemParanormal';
+}
+
+function Ordem_Inst() {
+    window.location.href = 'https://www.instagram.com/ordemparanormal/';
+}
+
+function Ordem_Face() {
+    window.location.href = 'https://www.facebook.com/OrdemParanormal/?locale=pt_BR';
+}
+
+function Ordem_Disc() {
+    window.location.href = 'https://discord.com/invite/STknVRA8bG';
+}
+
+// creator
+
+function inst(){
+    var Link = "https://www.instagram.com/lucas.bf_gts/";
+    window.open(Link, "_blank");
+}
+
+function disc(){
+    var Link = "https://discord.com/users/733748806771933254";
+    window.open(Link, "_blank");
+}
+
+function git(){
+    var Link = "https://github.com/LucasfcBastos";
+    window.open(Link, "_blank");
+}
+
+function link(){
+    var Link = "https://linkedin.com/in/lucas-bastos-811a172a7";
+    window.open(Link, "_blank");
+}
